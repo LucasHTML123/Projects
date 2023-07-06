2983df31a11d9d5b9e70a47510e24c43ea2e2486
Lucas <tucaskohlermfa@gmail.com>
2023-07-06 15:09:54 -0300

    updates

--- Audios/assets/js/add.js
+++ Audios/assets/js/add.js
@@ -27,7 +27,7 @@ function win() {
         fileInputBox.classList.add('outline')
     })
     
-    fileInputBox.addEventListener('dragleave', () => {
+    fileInputBox.addEventListener('dragleave', e => {
         fileInputBox.classList.remove('outline')
         e.preventDefault()
     })
@@ -54,13 +54,21 @@ function linux () {
     fileInput.addEventListener('change', e => {
         const text = document.querySelector('.text')
         const file = e.target.files[0]
+        const fileName = file.name
         const fileType = file.type
         if (fileType !== 'audio/mpeg') return alert('Favor selecionar um arquivo de aúdio .mp3!')
-        const fileName = file.name
+        if (localStorage.getItem(fileName) !== null) return alert('Favor mudar o nome do arquivo! Um arquivo com o mesmo nome já foi encontrado.')
         const i = document.createElement('i')
         i.classList.add('fa-solid', 'fa-file-audio', 'fa-2xl')
         i.style.lineHeight = '1'
         fileInputBox.insertBefore(i, text)
         text.innerText = fileName
+
+        handleEvent(file, fileName)
     })
+
+    function handleEvent (file, filename) {
+        const url = URL.createObjectURL(file)
+        localStorage.setItem(filename, url)
+    }
 }
